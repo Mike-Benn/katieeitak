0faacfe9688688a5f8a446a81fc6ea15effc6d63
Mike-Benn <mbenn49@wgu.edu>
2026-05-04 06:54:52 -0400

    Fix missing Field component bugs

---
+++ packages/frontend/src/components/Forms/Fields/SelectField/SelectField.tsx
@@ -0,0 +1,54 @@
+import { Check, ChevronDown } from 'lucide-react';
+import { Select, Field } from '@base-ui/react';
+import { useFieldContext } from '@/hooks/useAppForm';
+import type { SelectOption } from '@katieeitak/shared';
+
+interface SelectFieldProps {
+  items: SelectOption[];
+  description?: string;
+  label?: string;
+}
+
+export function SelectField({ items, description = '', label = '' }: SelectFieldProps) {
+  const field = useFieldContext<string>();
+  const hasError = field.state.meta.errors.length > 0;
+  return (
+    <Field.Root>
+      {label && <Field.Label className="font-semibold">{label}</Field.Label>}
+      <Select.Root
+        items={items}
+        value={field.state.value}
+        onValueChange={(value) => field.handleChange(value ?? '')}
+      >
+        <Select.Trigger className="flex rounded-sm border border-muted-border bg-muted-input select-none">
+          <Select.Value placeholder="Select type" className="data-placeholder:opacity-60" />
+          <Select.Icon>
+            <ChevronDown />
+          </Select.Icon>
+        </Select.Trigger>
+        <Select.Portal>
+          <Select.Positioner className="select-none z-10" alignItemWithTrigger={false}>
+            <Select.Popup className="group min-w-(--anchor-width) origin-(--transform-origin) bg-clip-padding border border-muted-border rounded-sm bg-[canvas] shadow-lg shadow-gray-200 transition-[transform,scale,opacity] data-ending-style:scale-90 data-ending-style:opacity-0">
+              <Select.List className="relative py-1 scroll-py-6 overflow-y-auto max-h-(--available-height)">
+                {items.map((item) => (
+                  <Select.Item
+                    key={item.label}
+                    value={item.value}
+                    className="grid cursor-default grid-cols-[0.75rem_1fr] items-center gap-2 py-2 pr-4 pl-2.5 leading-4 select-none data-highlighted:relative data-highlighted:z-0 data-highlighted:text-gray-50 data-highlighted:before:absolute data-highlighted:before:inset-x-1 data-highlighted:before:inset-y-0 data-highlighted:before:z-[-1] data-highlighted:before:rounded-sm data-highlighted:before:bg-gray-900"
+                  >
+                    <Select.ItemIndicator className="col-start-1">
+                      <Check size={16} />
+                    </Select.ItemIndicator>
+                    <Select.ItemText className="col-start-2">{item.label}</Select.ItemText>
+                  </Select.Item>
+                ))}
+              </Select.List>
+            </Select.Popup>
+          </Select.Positioner>
+        </Select.Portal>
+      </Select.Root>
+      {hasError && <Field.Error>{field.state.meta.errors[0]}</Field.Error>}
+      {description && <Field.Description>{description}</Field.Description>}
+    </Field.Root>
+  );
+}
--- packages/frontend/src/components/Forms/Fields/SliderField/SliderField.tsx
+++ packages/frontend/src/components/Forms/Fields/SliderField/SliderField.tsx
@@ -1,11 +1,12 @@
-import { Slider } from '@base-ui/react';
+import { Slider, Field } from '@base-ui/react';
 import { useFieldContext } from '@/hooks/useAppForm';
 
 interface SliderFieldProps {
   min: number;
   max: number;
   step: number;
-  label: string;
+  description?: string;
+  label?: string;
   minLabel?: string;
   maxLabel?: string;
 }
@@ -14,15 +15,16 @@ export function SliderField({
   min,
   max,
   step,
-  label,
+  description = '',
+  label = '',
   minLabel = '',
   maxLabel = '',
 }: SliderFieldProps) {
   const field = useFieldContext<number>();
-
+  const hasError = field.state.meta.errors.length > 0;
   return (
-    <div className="flex flex-col gap-2">
-      <h2 className="font-semibold">{label}</h2>
+    <Field.Root>
+      {label && <Field.Label className="font-semibold">{label}</Field.Label>}
       <Slider.Root
         className="flex gap-4"
         min={min}
@@ -40,24 +42,26 @@ export function SliderField({
               className="group relative size-4 rounded-full bg-white outline-1 outline-gray-300 select-none data-focus-visible:outline-2 ata-focus-visible:outline-blue-800"
             >
               <Slider.Value
-                className="absolute -top-6.75 left-1/2 -translate-x-1/2 
-                    rounded bg-gray-800 px-1.5 py-0.5 text-xs text-white whitespace-nowrap
-                    w-6.5 text-center
-                    after:absolute after:content-[''] 
-                    after:left-1/2 after:-translate-x-1/2
-                    after:top-full
-                    after:border-4 after:border-transparent after:border-t-gray-800
-                    opacity-0 transition-opacity duration-200
-                    group-hover:opacity-100 
-                    group-data-dragging:opacity-100 
-                    group-data-focus-visible:opacity-100
-                "
+                className="absolute -top-6.75 left-1/2 -translate-x-1/2
+                      rounded bg-gray-800 px-1.5 py-0.5 text-xs text-white whitespace-nowrap
+                      w-6.5 text-center
+                      after:absolute after:content-['']
+                      after:left-1/2 after:-translate-x-1/2
+                      after:top-full
+                      after:border-4 after:border-transparent after:border-t-gray-800
+                      opacity-0 transition-opacity duration-200
+                      group-hover:opacity-100
+                      group-data-dragging:opacity-100
+                      group-data-focus-visible:opacity-100
+                  "
               />
             </Slider.Thumb>
           </Slider.Track>
         </Slider.Control>
         <span>{maxLabel}</span>
       </Slider.Root>
-    </div>
+      {hasError && <Field.Error>{field.state.meta.errors[0]}</Field.Error>}
+      {description && <Field.Description>{description}</Field.Description>}
+    </Field.Root>
   );
 }
