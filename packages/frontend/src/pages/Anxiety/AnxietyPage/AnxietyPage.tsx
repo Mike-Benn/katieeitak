0e95f7d316485e3b6d4bdcd2dc59f13ce6dfe930
Mike-Benn <mbenn49@wgu.edu>
2026-05-11 07:15:51 -0400

    Add stale time to anxiety event query

--- packages/frontend/src/pages/Anxiety/AnxietyPage/AnxietyPage.tsx
+++ packages/frontend/src/pages/Anxiety/AnxietyPage/AnxietyPage.tsx
@@ -1,10 +1,20 @@
 import { PageWrapper } from '@/components/PageWrapper';
 import { Link } from '@tanstack/react-router';
 import { CalendarPlus } from 'lucide-react';
+import { useAnxietyEvents } from '@/hooks/useAnxietyEvents';
+import { SvgSpinner } from '@/components/Loading/SvgSpinner';
+import { toast } from 'sonner';
 
 export function AnxietyPage() {
+  const { isPending, isError, isFetching, isLoading, data, error } = useAnxietyEvents();
+  if (isError) {
+    toast.error('There was an error fetching Anxiety Events');
+  }
+  if (!isFetching && !isLoading) {
+    console.log(data);
+  }
   return (
-    <PageWrapper className="p-6 flex flex-col">
+    <PageWrapper className="p-6 flex flex-col gap-2">
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-2xl font-semibold">Events summary</h1>
         <Link to="/anxiety/new">
@@ -12,6 +22,9 @@ export function AnxietyPage() {
             <CalendarPlus />
           </div>
         </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center border border-red-600 min-h-123">
+        {(isPending || isLoading || isFetching) && <SvgSpinner size="h-16 w-16" />}
       </div>
     </PageWrapper>
   );
