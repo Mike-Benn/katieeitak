bda711af812d11fa883c21f7d202be30b6449717
Mike-Benn <mbenn49@wgu.edu>
2026-05-11 05:18:42 -0400

    Create getter query hook for anxiety events

---
+++ packages/frontend/src/hooks/useAnxietyEvents.ts
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+import { api } from '@/api/api';
+
+export function useAnxietyEvents() {
+  return useQuery({
+    queryKey: ['anxietyEvents'],
+    queryFn: ({ signal }) => api.getAnxietyEvents(signal),
+  });
+}
