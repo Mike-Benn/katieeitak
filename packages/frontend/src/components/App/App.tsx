a15e3f2991255f83e0a7907b5a770ac682cd5e76
Mike-Benn <mbenn49@wgu.edu>
2026-05-11 07:14:39 -0400

    Add Toaster to app

--- packages/frontend/src/components/App/App.tsx
+++ packages/frontend/src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import { router } from '@/routes/router';
 import { RouterProvider } from '@tanstack/react-router';
+import { Toaster } from 'sonner';
 
 export function App() {
   return (
-    <div className="app-wrapper flex flex-col relative">
-      <RouterProvider router={router} />
-    </div>
+    <>
+      <Toaster />
+      <div className="app-wrapper flex flex-col relative">
+        <RouterProvider router={router} />
+      </div>
+    </>
   );
 }
