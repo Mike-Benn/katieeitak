fd92980ad91ed91fe4e0257f4a5452cab0112c8a
Mike-Benn <mbenn49@wgu.edu>
2026-05-11 07:15:20 -0400

    Add event type and fix date schema bug

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -9,3 +9,4 @@ export {
 export type { AnxietyEventBody } from './schemas/forms/bodies.js';
 export { AnxietyEventBodySchema } from './schemas/forms/bodies.js';
 export type { AnxietyEvent } from './schemas/dtos/anxietyEvent.js';
+export type { AnxietyEventType } from './schemas/forms/validators.js';
