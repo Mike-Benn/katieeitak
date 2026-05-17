8bb5a54e11b350bd381a85ae3119a5e3a89b4c78
Mike-Benn <mbenn49@wgu.edu>
2026-05-17 03:09:08 -0400

    Add db config for dates and timestamps

--- packages/backend/src/db/db.ts
+++ packages/backend/src/db/db.ts
@@ -1,5 +1,13 @@
-import { Pool } from 'pg';
+import { Pool, types } from 'pg';
 import { BACKEND_ENV } from '@/env.js';
+
+types.setTypeParser(1082, function (stringValue) {
+  return new Date(stringValue).toISOString();
+});
+
+types.setTypeParser(1184, function (stringValue) {
+  return new Date(stringValue).toISOString();
+});
 
 const pool = new Pool({
   connectionString: BACKEND_ENV.DB_URL,
