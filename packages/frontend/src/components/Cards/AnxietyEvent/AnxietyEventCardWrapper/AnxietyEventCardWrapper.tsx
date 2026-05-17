53aea6400c7c017fd14b500aaa2cb7f4faf80b18
Mike-Benn <mbenn49@wgu.edu>
2026-05-17 04:59:07 -0400

    Add AnxietyEventCardWrapper to manage list of event cards

---
+++ packages/frontend/src/components/Cards/AnxietyEvent/AnxietyEventCardWrapper/AnxietyEventCardWrapper.tsx
@@ -0,0 +1,50 @@
+import { useAnxietyEvents } from '@/hooks/useAnxietyEvents';
+import { toast } from 'sonner';
+import { SvgSpinner } from '@/components/Loading/SvgSpinner';
+import { Ghost } from 'lucide-react';
+import { AnxietyEventCard } from '@/components/Cards/AnxietyEvent/AnxietyEventCard/AnxietyEventCard';
+import _ from 'lodash';
+
+export function AnxietyEventCardWrapper() {
+  const { isPending, isError, isFetching, isLoading, isSuccess, data, error } = useAnxietyEvents();
+  if (isError) {
+    toast.error('There was an error fetching Anxiety Events');
+  }
+  if (!isFetching && !isLoading) {
+    console.log(data);
+  }
+
+  const isContentReady = !isPending && !isLoading && !isFetching;
+
+  if (!isContentReady) {
+    return (
+      <div className="flex flex-col flex-1 items-center justify-center bg-white">
+        <SvgSpinner size="h-16 w-16" />
+      </div>
+    );
+  }
+
+  if (isSuccess && data.length < 1) {
+    return (
+      <div className="flex flex-col flex-1 items-center justify-center bg-white">
+        <Ghost size={42} />
+        <span>No events found.</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col flex-1 gap-6">
+      {isSuccess &&
+        data.map((event) => (
+          <AnxietyEventCard
+            key={event.id}
+            eventType={_.capitalize(event.event_type)}
+            dateString={event.date_occurred}
+            anxietyLevel={event.anxiety_level}
+            excitementLevel={event.excitement_level}
+          />
+        ))}
+    </div>
+  );
+}
