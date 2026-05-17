63ab56e03cc55f782b2e3585774780236915c0a0
Mike-Benn <mbenn49@wgu.edu>
2026-05-17 04:57:26 -0400

    Create AnxietyEvent card

--- packages/frontend/src/components/Cards/AnxietyEvent/AnxietyEventCard/AnxietyEventCard.tsx
+++ packages/frontend/src/components/Cards/AnxietyEvent/AnxietyEventCard/AnxietyEventCard.tsx
@@ -1,7 +1,8 @@
-import { PartyPopper, Meh, Clock } from 'lucide-react';
-
+import { PartyPopper, Meh, Frown, Smile, Clock } from 'lucide-react';
+import { formatInTimeZone } from 'date-fns-tz';
+import type { ReactNode } from 'react';
 interface AnxietyEventCardProps {
-  date?: string;
+  dateString?: string;
   anxietyLevel?: number;
   excitementLevel?: number;
   eventType?: string;
@@ -9,22 +10,33 @@ interface AnxietyEventCardProps {
 
 export function AnxietyEventCard({
   eventType = 'Unknown',
-  date = 'Unknown',
+  dateString,
   anxietyLevel = -1,
   excitementLevel = -1,
 }: AnxietyEventCardProps) {
+  const date = dateString ? formatInTimeZone(dateString, 'UTC', 'MMM dd, yyyy') : 'Unknown';
+  const anxietyIcon: ReactNode =
+    anxietyLevel <= 3 ? (
+      <Smile size={14} />
+    ) : anxietyLevel <= 6 ? (
+      <Meh size={14} />
+    ) : (
+      <Frown size={14} />
+    );
   return (
-    <div className="flex flex-col shadow-md rounded-md p-6">
+    <div className="flex flex-col shadow-md rounded-md p-6 bg-white">
       <div className="flex flex-row justify-between items-center">
         <span className="font-semibold text-lg">{eventType}</span>
-        <div className="flex flex-row gap-1 items-center">
-          <Clock color="#d7d7d7" size={16} />
-          <span className="text-gray-400 text-sm">{date}</span>
-        </div>
+        {date && (
+          <div className="flex flex-row gap-1 items-center">
+            <Clock color="#d7d7d7" size={16} />
+            <span className="text-gray-400 text-sm">{date}</span>
+          </div>
+        )}
       </div>
       <div className="flex flex-row gap-2">
         <div className="rounded-md p-1 flex flex-row gap-1 items-center bg-muted-input shadow-sm">
-          <Meh size={14} />
+          {anxietyIcon}
           <span className="text-sm">{anxietyLevel}</span>
         </div>
         <div className="rounded-md p-1 flex flex-row gap-1 items-center bg-muted-input shadow-sm">
---
+++ packages/frontend/src/components/Cards/AnxietyEvent/AnxietyEventCard/index.tsx
@@ -0,0 +1,1 @@
+export { AnxietyEventCard } from './AnxietyEventCard';
