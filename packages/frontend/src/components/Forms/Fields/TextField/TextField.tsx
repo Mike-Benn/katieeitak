f7a614685c25856eb758029299d395e48fcc059a
Mike-Benn <mbenn49@wgu.edu>
2026-05-17 05:17:12 -0400

    Create text field component

---
+++ packages/frontend/src/components/Forms/Fields/TextField/TextField.tsx
@@ -0,0 +1,37 @@
+import { useFieldContext } from '@/hooks/useAppForm';
+import { Field, Input } from '@base-ui/react';
+
+interface TextFieldProps {
+  label?: string;
+  placeholder?: string;
+  description?: string;
+  maxLength?: number;
+}
+
+export function TextField({
+  label = '',
+  placeholder = '',
+  description = '',
+  maxLength,
+}: TextFieldProps) {
+  const field = useFieldContext<string>();
+  const hasError = field.state.meta.errors.length > 0;
+
+  return (
+    <Field.Root className="flex flex-col gap-1">
+      {label && <Field.Label className="font-semibold">{label}</Field.Label>}
+      <Input
+        placeholder={placeholder}
+        value={field.state.value}
+        onChange={(e) => field.handleChange(e.target.value)}
+        className="border border-muted-border rounded-sm bg-muted-input pl-3 pr-3 pt-2 pb-2"
+        type="text"
+        maxLength={maxLength}
+      />
+      {hasError && (
+        <span className="text-red-500 text-sm">{field.state.meta.errors[0].message}</span>
+      )}
+      {description && <Field.Description>{description}</Field.Description>}
+    </Field.Root>
+  );
+}
