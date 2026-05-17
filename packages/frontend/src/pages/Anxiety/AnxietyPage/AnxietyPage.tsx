5a8c92aa4bc670fcde0e90560656cbd231b1b207
Mike-Benn <mbenn49@wgu.edu>
2026-05-17 04:59:35 -0400

    Add pages for Anxiety and NewAnxiety events

--- packages/frontend/src/pages/Anxiety/AnxietyPage/AnxietyPage.tsx
+++ packages/frontend/src/pages/Anxiety/AnxietyPage/AnxietyPage.tsx
@@ -1,31 +1,20 @@
 import { PageWrapper } from '@/components/PageWrapper';
 import { Link } from '@tanstack/react-router';
 import { CalendarPlus } from 'lucide-react';
-import { useAnxietyEvents } from '@/hooks/useAnxietyEvents';
-import { SvgSpinner } from '@/components/Loading/SvgSpinner';
-import { toast } from 'sonner';
+import { AnxietyEventCardWrapper } from '@/components/Cards/AnxietyEvent/AnxietyEventCardWrapper';
 
 export function AnxietyPage() {
-  const { isPending, isError, isFetching, isLoading, data, error } = useAnxietyEvents();
-  if (isError) {
-    toast.error('There was an error fetching Anxiety Events');
-  }
-  if (!isFetching && !isLoading) {
-    console.log(data);
-  }
   return (
-    <PageWrapper className="p-6 flex flex-col gap-2">
+    <PageWrapper className="p-6 flex flex-col min-h-[calc(100dvh-var(--nav-height))] gap-6 bg-muted-bg">
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-2xl font-semibold">Events summary</h1>
         <Link to="/anxiety/new">
-          <div className="border border-muted-border p-2 rounded-md bg-muted-input">
+          <div className="border border-muted-border p-2 rounded-md bg-muted-input shadow-md">
             <CalendarPlus />
           </div>
         </Link>
       </div>
-      <div className="flex flex-col items-center justify-center border border-red-600 min-h-123">
-        {(isPending || isLoading || isFetching) && <SvgSpinner size="h-16 w-16" />}
-      </div>
+      <AnxietyEventCardWrapper />
     </PageWrapper>
   );
 }
--- packages/frontend/src/pages/Anxiety/NewAnxietyEventPage/NewAnxietyEventPage.tsx
+++ packages/frontend/src/pages/Anxiety/NewAnxietyEventPage/NewAnxietyEventPage.tsx
@@ -1,21 +1,56 @@
+import { api } from '@/api/api';
 import { PageWrapper } from '@/components/PageWrapper';
 import { useAppForm } from '@/hooks/useAppForm';
-import { anxietyEventTypeOptions } from '@katieeitak/shared';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from '@tanstack/react-router';
+import { anxietyEventTypeOptions, AnxietyEventBodySchema } from '@katieeitak/shared';
+import { SubmitButton } from '@/components/Buttons/SubmitButton';
+import { toast } from 'sonner';
 
 export function NewAnxietyEventPage() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { mutate, isPending } = useMutation({
+    mutationFn: api.submitAnxietyEvent,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['anxietyEvents'] });
+      void navigate({ to: '/anxiety' });
+    },
+    onError: async () => {
+      toast.error('There was an error submitting, please try again.');
+    },
+  });
   const form = useAppForm({
     defaultValues: {
       anxietyLevel: 5,
       excitementLevel: 5,
       eventType: '',
       eventNotes: '',
-      eventDate: undefined as Date | undefined,
+      eventDate: undefined as string | undefined,
+    },
+    onSubmit: async ({ value }) => {
+      const parsedValue = AnxietyEventBodySchema.safeParse(value);
+      if (parsedValue.success) {
+        mutate(parsedValue.data);
+      } else {
+        throw new Error('Submit error');
+      }
+    },
+    validators: {
+      onSubmit: AnxietyEventBodySchema,
     },
   });
+
   return (
     <PageWrapper className="p-6 gap-6">
       <h1 className="text-2xl font-semibold">Create new event</h1>
-      <form className="flex flex-col gap-8">
+      <form
+        className="flex flex-col gap-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          void form.handleSubmit();
+        }}
+      >
         <form.AppField
           name="eventDate"
           children={(field) => <field.SelectDateField label="Event Date" />}
@@ -54,8 +89,11 @@ export function NewAnxietyEventPage() {
         />
         <form.AppField
           name="eventNotes"
-          children={(field) => <field.TextAreaField label="Notes" />}
+          children={(field) => <field.TextAreaField label="Notes" isRequired={false} />}
         />
+        <div className="flex flex-row justify-end">
+          <SubmitButton isSubmitting={isPending} />
+        </div>
       </form>
     </PageWrapper>
   );
