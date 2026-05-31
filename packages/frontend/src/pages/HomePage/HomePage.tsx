5c74a38a08c86e0bda8903bb02033626810c3086
Mike-Benn <mbenn49@wgu.edu>
2026-05-31 05:13:05 -0400

    Delete unused button component

--- packages/frontend/src/pages/HomePage/HomePage.tsx
+++ packages/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,9 @@
-import { LogoutButton } from '@/components/Buttons/LogoutButton';
 import { PageWrapper } from '@/components/PageWrapper';
 
 export function HomePage() {
   return (
     <PageWrapper>
       <h1>Hey</h1>
-      <LogoutButton />
     </PageWrapper>
   );
 }
