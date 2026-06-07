50ef3d5a4667436f18a7d8992473ceeedc732abb
Mike-Benn <mbenn49@wgu.edu>
2026-06-07 05:11:06 -0400

    Fix book schema bugs

--- packages/shared/src/schemas/dtos/book.ts
+++ packages/shared/src/schemas/dtos/book.ts
@@ -1,11 +1,18 @@
 import { z } from 'zod';
 
 export const BookSchema = z.object({
-  author_key: z.array(z.string()),
-  author_name: z.array(z.string()),
-  cover_i: z.number().int().positive(),
+  author_key: z.array(z.string()).optional(),
+  author_name: z.array(z.string()).optional(),
+  cover_i: z.number().int().positive().optional(),
   key: z.string(),
   title: z.string(),
-  first_publish_year: z.number().int().positive(),
+  first_publish_year: z.number().int().positive().optional(),
 });
 export type Book = z.infer<typeof BookSchema>;
+
+export const BookSearchResultsSchema = z.object({
+  num_found: z.number().int().nonnegative(),
+  books: z.array(BookSchema),
+});
+
+export type BookSearchResults = z.infer<typeof BookSearchResultsSchema>;
