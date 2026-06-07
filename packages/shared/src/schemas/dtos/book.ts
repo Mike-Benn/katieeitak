501300aba830b0d9b8aefe8cf9675eefe3e064aa
Mike-Benn <mbenn49@wgu.edu>
2026-06-07 01:23:07 -0400

    Fix bug in book schema

--- packages/shared/src/schemas/dtos/book.ts
+++ packages/shared/src/schemas/dtos/book.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const BookSchema = z.object({
-  author_key: [z.string()],
-  author_name: [z.string()],
+  author_key: z.array(z.string()),
+  author_name: z.array(z.string()),
   cover_i: z.number().int().positive(),
   title: z.string(),
   first_publish_year: z.number().int().positive(),
