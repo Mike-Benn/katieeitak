79c00733d6cdfbeb871ae9c6951deeef3febca4e
Mike-Benn <mbenn49@wgu.edu>
2026-06-07 01:25:07 -0400

    Create books route handling

--- packages/backend/src/api/v1/router.ts
+++ packages/backend/src/api/v1/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { identityRouter } from './features/identity/entry-points/router.js';
 import { anxietyRouter } from '@/api/v1/features/anxiety/entry-points/router.js';
+import { bookRouter } from '@/api/v1/features/books/router.js';
 
 const v1Router: Router = Router();
 
 v1Router.use('/auth', identityRouter);
 v1Router.use('/anxiety', anxietyRouter);
+v1Router.use('/books', bookRouter);
 
 export { v1Router };
