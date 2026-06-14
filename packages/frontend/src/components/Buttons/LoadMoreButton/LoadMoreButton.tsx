48b458ed95e69c5d2b09a59b55c17e5bffe5328f
Mike-Benn <mbenn49@wgu.edu>
2026-06-14 02:16:02 -0400

    Create load more button for paginated results

---
+++ packages/frontend/src/components/Buttons/LoadMoreButton/LoadMoreButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@base-ui/react';
+import { SvgSpinner } from '@/components/Loading/SvgSpinner';
+
+interface LoadMoreButtonProps {
+  onClick: () => void;
+  isFetchingNextPage: boolean;
+  className?: string;
+}
+
+export function LoadMoreButton({
+  onClick,
+  isFetchingNextPage,
+  className = '',
+}: LoadMoreButtonProps) {
+  return (
+    <Button
+      type="button"
+      onClick={onClick}
+      className={`flex items-center justify-center pt-2 pb-2 ${!isFetchingNextPage && 'border border-black bg-white rounded-md shadow-sm'} ${className}`}
+    >
+      {isFetchingNextPage ? <SvgSpinner size="h-6 w-6" color="black" /> : 'Load More'}
+    </Button>
+  );
+}
