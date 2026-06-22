2d3db967f4927115e433edfe82350ad1c4eeb7e8
Mike-Benn <mbenn49@wgu.edu>
2026-06-22 04:23:23 -0400

    Rework book profile component

--- packages/frontend/src/pages/Books/BookProfile/BookProfile.tsx
+++ packages/frontend/src/pages/Books/BookProfile/BookProfile.tsx
@@ -1,39 +1,52 @@
 import { PageWrapper } from '@/components/PageWrapper';
-import { useQuery } from '@tanstack/react-query';
 import { useParams } from '@tanstack/react-router';
-import { api } from '@/api/api';
 import { SvgSpinner } from '@/components/Loading/SvgSpinner';
 import { useEffect } from 'react';
 import { isAxiosError } from 'axios';
 import { NotFound } from '@/pages/NotFound';
 import { toast } from 'sonner';
 import { BookProfileCard } from '@/components/Cards/Books/BookProfileCard';
+import { useBookProfile } from '@/hooks/queries/useBookProfile';
+import { SaveBookDialog } from '@/components/Dialogs/SaveBookDialog';
 
 export function BookProfile() {
   const { key } = useParams({ from: '/MainLayout/books/$key' });
-  const { data, isFetching, isError, error } = useQuery({
-    queryKey: ['fetchBook', key],
-    queryFn: ({ signal }) => api.getBookByKey({ key, signal }),
-    refetchOnWindowFocus: false,
-  });
+  const { bookProfileQuery, markedBookProfileQuery } = useBookProfile({ bookKey: key });
+  const isGlobalLoading = bookProfileQuery.isLoading || markedBookProfileQuery.isLoading;
   useEffect(() => {
-    if (isError && isAxiosError(error) && error.response?.status !== 404) {
+    if (
+      bookProfileQuery.isError &&
+      isAxiosError(bookProfileQuery.error) &&
+      bookProfileQuery.error.response?.status !== 404
+    ) {
       toast.error(
         'There was an error loading the requested book, please wait a moment and try again.',
       );
     }
-  }, [isError]);
-  if (isError && isAxiosError(error) && error.response?.status === 404) {
+  }, [bookProfileQuery.isError]);
+  if (
+    bookProfileQuery.isError &&
+    isAxiosError(bookProfileQuery.error) &&
+    bookProfileQuery.error.response?.status === 404
+  ) {
     return <NotFound />;
   }
   return (
     <PageWrapper className="p-6 gap-6">
-      {isFetching && (
+      {isGlobalLoading && (
         <div className="flex h-full justify-center items-center flex-1">
           <SvgSpinner />
         </div>
       )}
-      {data && !isFetching && <BookProfileCard bookProfile={data} />}
+      {!isGlobalLoading && bookProfileQuery.isSuccess && (
+        <BookProfileCard bookProfile={bookProfileQuery.data} />
+      )}
+      {!isGlobalLoading && markedBookProfileQuery.isSuccess && bookProfileQuery.isSuccess && (
+        <SaveBookDialog
+          markedBookProfileData={markedBookProfileQuery.data}
+          bookProfileData={bookProfileQuery.data}
+        />
+      )}
     </PageWrapper>
   );
 }
