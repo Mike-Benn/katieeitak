4d4fd8c07abe66b3fd114f728b57cd9c1cdaa7ab
Mike-Benn <mbenn49@wgu.edu>
2026-06-22 04:15:19 -0400

    Add conditional check

--- packages/frontend/src/utils/getStarFill/getStarFill.ts
+++ packages/frontend/src/utils/getStarFill/getStarFill.ts
@@ -1,11 +1,13 @@
 interface GetStarFillParams {
   starIndex: number;
-  currRating: number;
+  currRating: number | undefined;
 }
 type StarFill = '#f5a623' | 'url(#half-fill)' | 'transparent';
 
 export function getStarFill({ starIndex, currRating }: GetStarFillParams): StarFill {
-  if (currRating >= starIndex) return '#f5a623';
-  if (currRating >= starIndex - 0.5) return 'url(#half-fill)';
+  if (currRating) {
+    if (currRating >= starIndex) return '#f5a623';
+    if (currRating >= starIndex - 0.5) return 'url(#half-fill)';
+  }
   return 'transparent';
 }
