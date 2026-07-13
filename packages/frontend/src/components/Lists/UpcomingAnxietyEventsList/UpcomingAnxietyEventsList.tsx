b513e18b9af259063f6203d8cdced1f170c8c9d4
Mike-Benn <mbenn49@wgu.edu>
2026-07-13 00:52:18 -0400

    Create list to display upcoming anxiety events

---
+++ packages/frontend/src/components/Lists/UpcomingAnxietyEventsList/UpcomingAnxietyEventsList.tsx
@@ -0,0 +1,32 @@
+import type { InfiniteData } from '@tanstack/react-query';
+import type { GetAnxietyEventsResponse } from '@katieeitak/shared';
+import { Ghost } from 'lucide-react';
+import React from 'react';
+import { AnxietyEventCard } from '@/components/Cards/AnxietyEvent/AnxietyEventCard';
+
+interface LatestAnxietyEventsListProps {
+  eventsResponse: InfiniteData<GetAnxietyEventsResponse, number>;
+}
+
+export function UpcomingAnxietyEventsList({ eventsResponse }: LatestAnxietyEventsListProps) {
+  if (eventsResponse.pages[0].num_found === 0) {
+    return (
+      <div className="flex flex-col flex-1 items-center justify-center">
+        <Ghost size={42} />
+        <span>No events found.</span>
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col gap-6">
+      {eventsResponse.pages.map((page, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <React.Fragment key={index}>
+          {page.anxietyEvents.map((anxietyEvent) => (
+            <AnxietyEventCard key={anxietyEvent.id} anxietyEvent={anxietyEvent} />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
