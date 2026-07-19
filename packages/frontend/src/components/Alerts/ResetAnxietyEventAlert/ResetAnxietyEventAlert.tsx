dc18e750d31e3bb332ea2b80f79209d793cbc095
Mike-Benn <mbenn49@wgu.edu>
2026-07-19 04:39:38 -0400

    Add reset anxiety event button

---
+++ packages/frontend/src/components/Alerts/ResetAnxietyEventAlert/ResetAnxietyEventAlert.tsx
@@ -0,0 +1,67 @@
+import { AlertDialog, Button } from '@base-ui/react';
+import { useState } from 'react';
+import { History } from 'lucide-react';
+import type { AnxietyEvent } from '@katieeitak/shared';
+import { useUncompleteAnxietyEventByIdMutation } from '@/hooks/mutations/useUncompleteAnxietyEventByIdMutation';
+import { toast } from 'sonner';
+
+interface ResetAnxietyEventAlertProps {
+  anxietyEvent: AnxietyEvent;
+}
+
+export function ResetAnxietyEventAlert({ anxietyEvent }: ResetAnxietyEventAlertProps) {
+  const [open, setOpen] = useState(false);
+  const { mutate } = useUncompleteAnxietyEventByIdMutation();
+  return (
+    <AlertDialog.Root
+      open={open}
+      onOpenChange={(open) => {
+        setOpen(open);
+      }}
+    >
+      <div className="flex h-full w-full justify-center items-center">
+        <AlertDialog.Trigger>
+          <History size={21} color="black" />
+        </AlertDialog.Trigger>
+      </div>
+      <AlertDialog.Portal>
+        <AlertDialog.Backdrop className="fixed inset-0 min-h-dvh bg-black opacity-20 transition-all duration-150 data-ending-style:opacity-0 data-starting-style:opacity-0 dark:opacity-70 supports-[-webkit-touch-callout:none]:absolute" />
+        <AlertDialog.Popup className="fixed top-1/2 left-1/2 w-96 max-w-[calc(100vw-3rem)] max-h-[90dvh] -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-lg bg-gray-50 text-gray-900 outline-1 outline-gray-200 transition-all duration-150 data-ending-style:scale-90 data-ending-style:opacity-0 data-starting-style:scale-90 data-starting-style:opacity-0 dark:outline-gray-300">
+          <div className="flex flex-col gap-4 px-6 py-4 shrink-0">
+            <div className="flex flex-col">
+              <h2 className="text-md font-semibold">Undo completion?</h2>
+              <span className="text-sm text-gray-400">Resets event to 'Upcoming' status.</span>
+            </div>
+
+            <div className="flex justify-end gap-2.5">
+              <Button
+                className="text-sm border border-black rounded-sm px-3 py-1"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                className="bg-black text-white text-sm rounded-sm px-3 py-1"
+                onClick={() => {
+                  mutate(
+                    { id: anxietyEvent.id },
+                    {
+                      onSuccess: () => {
+                        setOpen(false);
+                        toast.success('Anxiety event reset!');
+                      },
+                    },
+                  );
+                }}
+              >
+                Undo
+              </Button>
+            </div>
+          </div>
+        </AlertDialog.Popup>
+      </AlertDialog.Portal>
+    </AlertDialog.Root>
+  );
+}
