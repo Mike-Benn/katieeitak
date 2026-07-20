351ff744e3fc2c253adb27dda81e56ed91ef0b1d
Mike-Benn <mbenn49@wgu.edu>
2026-07-20 05:54:20 -0400

    Add logo link to app header, rework header styles

--- packages/frontend/src/components/Headers/AppHeader/AppHeader.tsx
+++ packages/frontend/src/components/Headers/AppHeader/AppHeader.tsx
@@ -1,16 +1,16 @@
-import { CircleUserRound } from 'lucide-react';
 import { NavDrawer } from '@/components/Drawers/NavDrawer';
+import { Link } from '@tanstack/react-router';
 
 export function AppHeader() {
   return (
-    <header className="flex flex-row justify-between items-center border-b border-lightgray p-4 pl-4 pr-4 bg-white">
-      <div className="flex flex-row gap-2">
+    <header className="flex flex-row justify-between items-center border-b border-lightgray py-2.5 pl-4 pr-4 bg-white">
+      <div className="flex flex-row gap-4">
         <NavDrawer />
-        <h1>Katieeitak</h1>
+        <Link to="/" aria-label="Go to homepage">
+          <img src="/app-logo.png" alt="Website logo" className="h-11.5 w-auto" />
+        </Link>
       </div>
-      <div>
-        <CircleUserRound size={20} />
-      </div>
+      <div></div>
     </header>
   );
 }
