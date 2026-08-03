a8b5add950f9b3f89937c9adccbe01bfcd7fc21f
Mike-Benn <mbenn49@wgu.edu>
2026-08-03 03:32:11 -0400

    Create hook for marking plates

---
+++ packages/frontend/src/hooks/mutations/useMarkPlateSeenMutation.ts
@@ -0,0 +1,27 @@
+import { api } from '@/api/api';
+import { type GetCurrentTripByUserIdResponse } from '@katieeitak/shared';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+export function useMarkPlateSeenMutation() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: api.markPlateSeen,
+    onError: () => {
+      toast.error('There was an error marking plate, please try again.');
+    },
+    onSuccess: async (data) => {
+      //await queryClient.resetQueries({ queryKey: ['current-trip'] });
+      queryClient.setQueryData<GetCurrentTripByUserIdResponse>(['current-trip'], (old) => {
+        if (!old) return old;
+        return old.map((plate) => {
+          if (plate.id !== data.plate_id) return plate;
+          return {
+            ...plate,
+            date_seen: data.date_seen,
+          };
+        });
+      });
+    },
+  });
+}
