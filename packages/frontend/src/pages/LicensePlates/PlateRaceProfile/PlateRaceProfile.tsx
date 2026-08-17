14bbd985ff7da127c45895a2ae63243d0b7d09d8
Mike-Benn <mbenn49@wgu.edu>
2026-08-17 16:28:03 -0400

    Create plate race profile component

---
+++ packages/frontend/src/pages/LicensePlates/PlateRaceProfile/PlateRaceProfile.tsx
@@ -0,0 +1,26 @@
+import { useTripData } from '@/hooks/queries/useTripData';
+import { useParams } from '@tanstack/react-router';
+import { PageWrapper } from '@/components/PageWrapper';
+import { LicensePlatesList } from '@/components/Lists/LicensePlatesList';
+import { MissingResourcePage } from '@/pages/MissingResourcePage/MissingResourcePage';
+import { isAxiosError } from 'axios';
+
+export function PlateRaceProfile() {
+  const { id } = useParams({ from: '/MainLayout/license-plates/$id' });
+  const tripQuery = useTripData({ tripId: id });
+
+  if (
+    !tripQuery.isFetching &&
+    tripQuery.isError &&
+    isAxiosError(tripQuery.error) &&
+    tripQuery.error.status === 404
+  ) {
+    return <MissingResourcePage backTo="/license-plates" />;
+  }
+
+  return (
+    <PageWrapper className="p-6 gap-6">
+      {!tripQuery.isFetching && <LicensePlatesList currentTrip={tripQuery.data} />}
+    </PageWrapper>
+  );
+}
