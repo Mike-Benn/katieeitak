896583382b6a5305385caea86388cee3ae76bc32
Mike-Benn <mbenn49@wgu.edu>
2026-08-23 05:12:28 -0400

    Add altText option

--- packages/frontend/src/utils/createReadableDate/createReadableDate.ts
+++ packages/frontend/src/utils/createReadableDate/createReadableDate.ts
@@ -3,12 +3,17 @@ import { isValid, format, parseISO } from 'date-fns';
 interface CreateReadableDateParams {
   dateString: string | null | undefined;
   dateStyle: 'MM/dd/yy';
+  altText?: string;
 }
 
-export function createReadableDate({ dateString, dateStyle }: CreateReadableDateParams) {
+export function createReadableDate({
+  dateString,
+  dateStyle,
+  altText = 'Unknown',
+}: CreateReadableDateParams) {
   if (!dateString) {
-    return 'Unknown';
+    return altText;
   }
   const parsedDateString = parseISO(dateString);
-  return isValid(parsedDateString) ? format(parsedDateString, dateStyle) : 'Unknown';
+  return isValid(parsedDateString) ? format(parsedDateString, dateStyle) : altText;
 }
