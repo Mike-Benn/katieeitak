5cdb3500776e15eda2d3200d3089e31301808542
Mike-Benn <mbenn49@wgu.edu>
2026-08-23 23:53:14 -0400

    Fix styling ugliness on smaller mobile devices on home dashboard

--- packages/frontend/src/pages/HomePage/HomePage.tsx
+++ packages/frontend/src/pages/HomePage/HomePage.tsx
@@ -4,35 +4,35 @@ import { Link } from '@tanstack/react-router';
 
 export function HomePage() {
   return (
-    <PageWrapper className="p-6">
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(140px,1fr))] gap-4 justify-center">
+    <PageWrapper>
+      <div className="grid grid-cols-2 gap-4 p-6">
         <Link
           to="/paycheck"
           className="no-underline visited:text-inherit flex flex-col bg-white aspect-square justify-center items-center shadow-md rounded-2xl p-6 gap-2"
         >
           <BadgeDollarSign size={36} />
-          <span className="font-semibold text-lg">Paycheck</span>
+          <span className="font-semibold text-lg text-center line-clamp-1">Paycheck</span>
         </Link>
         <Link
           to="/anxiety"
           className="no-underline visited:text-inherit flex flex-col bg-white aspect-square justify-center items-center shadow-md rounded-2xl p-6 gap-2"
         >
           <Leaf size={36} />
-          <span className="font-semibold text-lg">Anxiety</span>
+          <span className="font-semibold text-lg text-center line-clamp-1">Anxiety</span>
         </Link>
         <Link
           to="/books"
           className="no-underline visited:text-inherit flex flex-col bg-white aspect-square justify-center items-center shadow-md rounded-2xl p-6 gap-2"
         >
           <BookMarked size={36} />
-          <span className="font-semibold text-lg">Books</span>
+          <span className="font-semibold text-lg text-center line-clamp-1">Books</span>
         </Link>
         <Link
           to="/plate-races"
           className="no-underline visited:text-inherit flex flex-col bg-white aspect-square justify-center items-center shadow-md rounded-2xl p-6 gap-2"
         >
           <Car size={36} />
-          <span className="font-semibold text-lg text-center">Plate races</span>
+          <span className="font-semibold text-lg text-center line-clamp-1">Plate races</span>
         </Link>
       </div>
     </PageWrapper>
