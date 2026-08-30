125bbde310349d1cfd8647d79f23555fec1f202f
Mike-Benn <mbenn49@wgu.edu>
2026-08-30 05:38:45 -0400

    Remove dead link from HomePage

--- packages/frontend/src/pages/HomePage/HomePage.tsx
+++ packages/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import { PageWrapper } from '@/components/PageWrapper';
-import { BadgeDollarSign, Leaf, BookMarked, Car, Plane } from 'lucide-react';
+import { BadgeDollarSign, Leaf, BookMarked, Plane } from 'lucide-react';
 import { Link } from '@tanstack/react-router';
 
 export function HomePage() {
@@ -26,13 +26,6 @@ export function HomePage() {
         >
           <BookMarked size={36} />
           <span className="font-semibold text-lg text-center line-clamp-1">Books</span>
-        </Link>
-        <Link
-          to="/plate-races"
-          className="no-underline visited:text-inherit flex flex-col bg-white aspect-square justify-center items-center shadow-md rounded-2xl p-6 gap-2"
-        >
-          <Car size={36} />
-          <span className="font-semibold text-lg text-center line-clamp-1">Plate races</span>
         </Link>
         <Link
           to="/america"
